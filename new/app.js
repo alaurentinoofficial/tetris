b546b93448e398a100a1585929e63a1d371071a7
Anderson Laurentino <anderson@laurentino.me>
2020-09-22 10:43:54 -0300

    Create a Piece Facotry to build each one piece [I, O, L, J, S, Z, T] randomly

--- new/app.js
+++ new/app.js
@@ -5,51 +5,7 @@ let ctx;
 let canvas;
 
 function play() {
-    let l = new Piece(ctx, [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [1, 1, 1, 1],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
-    ], "#00c3ff");
-
-    let J = new Piece(ctx, [
-        [1, 0, 0],
-        [1, 1, 1],
-        [0, 0, 0]
-    ], "#ff6f00");
-
-    let L = new Piece(ctx, [
-        [0, 0, 1],
-        [1, 1, 1],
-        [0, 0, 0]
-    ], "#ff00b3");
-
-    let O = new Piece(ctx, [
-        [1, 1],
-        [1, 1]
-    ], "#fffb00");
-
-    let S = new Piece(ctx, [
-        [0, 1, 1],
-        [1, 1, 0],
-        [0, 0, 0]
-    ], "#ff0000");
-
-    let Z = new Piece(ctx, [
-        [1, 1, 0],
-        [0, 1, 1],
-        [0, 0, 0]
-    ], "#00ff5e");
-
-    let T = new Piece(ctx, [
-        [0, 1, 0],
-        [1, 1, 1],
-        [0, 0, 0]
-    ], "#c300ff");
-
-    let pieces = [l, J, L, O, S, Z, T]
-    let piece = pieces[Math.floor(Math.random() * (pieces.length))];
+    let piece = new PieceFactory(ctx);
 
     board = new Board(ctx);
     piece.clear();
