39f1a19c942dba30fb9ca3d2bbe9759791044371
Anderson Laurentino <anderson@laurentino.me>
2020-09-25 17:32:54 -0300

    Create a separed class to represent the Audio component of HTML

--- src/js/AudioMixer.js
+++ src/js/AudioMixer.js
@@ -1,27 +1,31 @@
+function AudioComponent(audioComponent) {
+    return {
+        component: audioComponent
+        , Play: function() {
+            audioComponent.play();
+        }
+        , Pause: function() {
+            audioComponent.pause();
+        }
+        , Stop: function () {
+            audioComponent.pause();
+            audioComponent.currentTime = 0;
+        }
+        , SetVolume: function (volume) {
+            audioComponent.volume = volume;
+        }
+        , SetLoop: function (state) {
+            audioComponent.loop = state;
+        } 
+    };
+}
+
 var AudioMixer = (function () {
     var instance;
     var audios = {}
 
     function AddAudio(name, audioComponent) {
-        audios[name] = {
-            component: audioComponent
-            , Play: function() {
-                audioComponent.play();
-            }
-            , Pause: function() {
-                audioComponent.pause();
-            }
-            , Stop: function () {
-                audioComponent.pause();
-                audioComponent.currentTime = 0;
-            }
-            , SetVolume: function (volume) {
-                audioComponent.volume = volume;
-            }
-            , SetLoop: function (state) {
-                audioComponent.loop = state;
-            } 
-        };
+        audios[name] = new AudioComponent(audioComponent);
     }
 
     function GetAudios() {
